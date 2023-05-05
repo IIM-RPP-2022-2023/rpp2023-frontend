5014e8aff4d8eca4633431059bceb36607715743
staka99 <marko.staka12@gmail.com>
2023-05-05 02:12:41 +0200

    Vezbe 08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './vozilo/vozilo.component';
+import { AutomobilComponent } from './vozilo/automobil/automobil.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArtiklComponent } from './artikl/artikl.component';
+import { DobavljacComponent } from './dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './stavka-porudzbine/stavka-porudzbine.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { AuthorComponent } from './core/author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/vozilo/automobil/automobil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-automobil',
+  templateUrl: './automobil.component.html',
+  styleUrls: ['./automobil.component.css']
+})
+export class AutomobilComponent {
+
+}
